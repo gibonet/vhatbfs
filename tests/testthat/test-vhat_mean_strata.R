709c389ed6b69027f27a06e9d444cb8ccb6f110d
gibo <gibo.gaf@gmail.com>
2024-03-22 14:34:35 +0100

    add two tests for vhat_mean_strata()

--- tests/testthat/test-vhat_mean_strata.R
+++ tests/testthat/test-vhat_mean_strata.R
@@ -1,5 +1,19 @@
 test_that("vhat_mean_strata works", {
   res <- vhat_mean_strata(d$w, d$w)
   res2 <- vhat_mean_strata(d$w, d$w, fpc = FALSE)
+  
+  # The estimated variance with fpc should be greater than without
   expect_gt(res, res2)
+  
+  # Verify that vhat_mean_strata works without weights
+  res <- vhat_mean_strata(d$w)
+  expect_false(is.na(res))
+  
+  # vhat_mean_strata works if data contains NA values
+  d2 <- d
+  d2$w[sample.int(nrow(d), size = 10)] <- NA
+  res <- vhat_mean_strata(d2$w, d2$w)
+  
+  expect_false(is.na(res))
+  
 })
