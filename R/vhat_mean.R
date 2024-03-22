8b4b6005f67dedbb58ebd06d573b80b2d24617d1
gibo <sandro.burri@ti.ch>
2024-03-22 13:46:06 +0100

    moved old stuff at the end of the script, add a note about nPSU (not used at the moment in vhat_mean())

--- R/vhat_mean.R
+++ R/vhat_mean.R
@@ -1,28 +1,3 @@
-# Come vhat_strata5 ma per la stima della varianza di una media (* 1 / Nhat^2)
-# No non va bene, devo includere anche una variabile numerica y (tipo l'affitto),
-# oltre al peso (che al momento è x)
-# vhat_mean_strata <- function(x, mh, Nh){
-#   k <- length(x)
-#   mh <- unique(mh)
-#   Nh <- unique(Nh)
-#   stopifnot(length(mh) == 1 || length(Nh) == 1)
-#   Nhat <- sum(x)
-#   primo_termine <- (mh - k) * (Nhat/mh)^2
-#   secondo_termine <- sum((x - Nhat/mh)^2)
-#   termine <- primo_termine + secondo_termine
-#   res <- mh/(mh - 1) * (1 - mh/Nh) * termine
-#   res / Nhat^2
-# }
-
-# Provo una variante senza fpc
-# vhat_mean_strata2 <- function(y, weights){
-#   ybar <- stats::weighted.mean(y, weights)
-#   Nhat <- sum(weights)
-#
-#   sigma2 <- sum(weights * (y - ybar)^2) / Nhat # * Nhat / (Nhat - 1)
-#   sigma2 / Nhat^2
-# }
-
 
 # Stima della varianza della stima di una media (di uno strato)
 
@@ -60,9 +35,7 @@ vhat_mean_strata <- function(y, weights, nPSU = NULL, na.rm = TRUE) {
 
 
 # Prova
-
-
-
+# nPSU is not used at the moment. Should it be different for each strata?
 vhat_mean <- function(y, weights, nPSU = NULL, strata = NULL) {
   stopifnot(length(y) == length(weights))
   
@@ -88,3 +61,32 @@ vhat_mean <- function(y, weights, nPSU = NULL, strata = NULL) {
 
 # vhat_mean(d$w, d$w, strata = NULL)
 # vhat_mean(d$w, d$w, strata = d$s)
+
+
+
+
+# Old stuff -------
+# Come vhat_strata5 ma per la stima della varianza di una media (* 1 / Nhat^2)
+# No non va bene, devo includere anche una variabile numerica y (tipo l'affitto),
+# oltre al peso (che al momento è x)
+# vhat_mean_strata <- function(x, mh, Nh){
+#   k <- length(x)
+#   mh <- unique(mh)
+#   Nh <- unique(Nh)
+#   stopifnot(length(mh) == 1 || length(Nh) == 1)
+#   Nhat <- sum(x)
+#   primo_termine <- (mh - k) * (Nhat/mh)^2
+#   secondo_termine <- sum((x - Nhat/mh)^2)
+#   termine <- primo_termine + secondo_termine
+#   res <- mh/(mh - 1) * (1 - mh/Nh) * termine
+#   res / Nhat^2
+# }
+
+# Provo una variante senza fpc
+# vhat_mean_strata2 <- function(y, weights){
+#   ybar <- stats::weighted.mean(y, weights)
+#   Nhat <- sum(weights)
+#
+#   sigma2 <- sum(weights * (y - ybar)^2) / Nhat # * Nhat / (Nhat - 1)
+#   sigma2 / Nhat^2
+# }
