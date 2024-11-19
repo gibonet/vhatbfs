3c951ee424fc8a3f1c377edfef13991ca703f96f
gibo <gibo.gaf@gmail.com>
2024-11-19 17:24:04 +0100

    section titles in examples

--- inst/examples.R
+++ inst/examples.R
@@ -3,15 +3,18 @@ library(vhatbfs)
 
 str(d)
 
+# Basic example (vector) ----
 sum(d$w)
 vhat3(x = d$w, mh = d$mh, Nh = d$Nh, strata = d$s)
 
+# Confidence interval (vector) --------
 sum(d$w) + c(-1, 1) *
   qnorm(0.975) *
   sqrt(
     vhat3(x = d$w, mh = d$mh, Nh = d$Nh, strata = d$s)
   )
 
+# Variance by group, with split and lapply ------
 lapply(
   split(d, f = d$xcat), 
   FUN = function(x) {
